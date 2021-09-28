ab573a2ef1bc6317f2a312d6f39e5cab743185f3
qimoraenjelika27 <90614833+qimoraenjelika27@users.noreply.github.com>
2021-09-29 01:17:55 +0700

    Add files via upload

---
+++ Java Script Dasar/loop-04.js
@@ -0,0 +1,35 @@
+/// Soal - 01
+/// Kamu diminta untuk membuat sebuah perulangan menggunakan for loop, untuk mencetak angka prima dari 1 ~ 100
+/// EDIT HERE
+for (let i = 2; i <= 100; i++) {
+    console.log(i)
+}
+
+
+/// Soal - 02
+/// Kamu diminta untuk menemukan bilangan prima ke-50, temukan bilangan tersebut menggunakan while loop 
+let primeCounter = 0;
+let fiftiethPrime;
+
+while (i < 50) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+    i++
+}
+
+/// EDIT HERE
+while (....) {}
+
+
+/// Soal - 03
+/// Kamu diminta untuk menemukan bilangan ganjil ke-50, temukan bilangan tersebut menggunakan do while loop
+let oddCounter = 0;
+let fiftiethOdd;
+
+for (let i = 0; i <= 50; i++) {
+
+}
+
+/// EDIT HERE
+do {.... } while (....)
